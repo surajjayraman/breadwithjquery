9ae70b506fe2db4a5195e98d1b1301453538e9d9
surajjayraman <suraj.jayraman@gmail.com>
2023-07-13 20:19:35 -0400

    Add new elements using jQuery

--- client.js
+++ client.js
@@ -1,13 +1,22 @@
-$(document).ready(() => {
+console.log(`Hello ${username}`);
 
-  console.log('Hello World');
+// using vanilla js
+// create a new li
+const myLi = document.createElement('li');
+// create a text node for the li
+const textNode = document.createTextNode('Four');
+myLi.append(textNode);
+const mainList = document.getElementById('main-list');
+mainList.append(myLi);
 
-  // using vanilla js
-  // create a new li
-  const myLi = document.createElement('li');
-  // create a text node for the li
-  const textNode = document.createTextNode('Four');
-  myLi.append(textNode);
-  const mainList = document.getElementById('main-list');
-  mainList.append(myLi);
-});
+console.log(jQuery);
+
+// using jQuery
+const $myLi = $('<li>Five</li>');
+
+// find something in the DOM that already exists
+const $mainList = $('#main-list');
+
+// add the newly created li to the main-list
+$mainList.append($myLi);
+
