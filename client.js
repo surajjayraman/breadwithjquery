eeb905860666e23efd558fba4662f752cc9822d4
surajjayraman <suraj.jayraman@gmail.com>
2023-07-14 12:25:32 -0400

    Add listener on button

--- client.js
+++ client.js
@@ -12,11 +12,47 @@ mainList.append(myLi);
 console.log(jQuery);
 
 // using jQuery
-const $myLi = $('<li>Five</li>');
+const $myLi = $('<li>').text('five with jQuery');
 
 // find something in the DOM that already exists
 const $mainList = $('#main-list');
 
 // add the newly created li to the main-list
 $mainList.append($myLi);
+
+$hello = $(`
+    <div>
+       <h2> Hello </h2>
+    </div>
+`);
+
+$mainList.append($hello);
+
+//
+// On button click, retrieve the value from the input and create a new li
+//
+
+// grab the button from the DOM
+const $button = $('#add-button');
+
+// add a click listener to the button
+$button.on('click', () => {
+  console.log('button got clicked');
+
+  // get the input field
+  const $input = $('#new-number');
+
+  // get the value from the input field
+  const inputContents = $input.val();
+    
+  // create a new li with the input contents
+  const newLi = $(`<li>${inputContents}</li>`);
+
+  // add the new li to the main-list
+  $mainList.prepend(newLi);
+
+  // clear the input field and set focus
+  $input.val('').focus();
+  // $input.focus();
+});
 
