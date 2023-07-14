fc0ef61416290e3d4a46b99e50df6dd11cf075f1
surajjayraman <suraj.jayraman@gmail.com>
2023-07-14 13:28:00 -0400

    Update js code with document ready

--- client.js
+++ client.js
@@ -1,58 +1,64 @@
-console.log(`Hello ${username}`);
+$(document).ready(() => {
+  console.log(`Hello ${username}`);
+  // using vanilla js
+  // create a new li
+  const myLi = document.createElement('li');
+  // create a text node for the li
+  const textNode = document.createTextNode('Four');
+  myLi.append(textNode);
+  const mainList = document.getElementById('main-list');
+  mainList.append(myLi);
 
-// using vanilla js
-// create a new li
-const myLi = document.createElement('li');
-// create a text node for the li
-const textNode = document.createTextNode('Four');
-myLi.append(textNode);
-const mainList = document.getElementById('main-list');
-mainList.append(myLi);
+  console.log(jQuery);
 
-console.log(jQuery);
+  // using jQuery
+  const $myLi = $('<li>').text('five with jQuery');
 
-// using jQuery
-const $myLi = $('<li>').text('five with jQuery');
+  // find something in the DOM that already exists
+  const $mainList = $('#main-list');
 
-// find something in the DOM that already exists
-const $mainList = $('#main-list');
+  // add the newly created li to the main-list
+  $mainList.append($myLi);
 
-// add the newly created li to the main-list
-$mainList.append($myLi);
-
-$hello = $(`
+  $hello = $(`
     <div>
        <h2> Hello </h2>
     </div>
 `);
 
-$mainList.append($hello);
+  $mainList.append($hello);
 
-//
-// On button click, retrieve the value from the input and create a new li
-//
+  //
+  // On button click, retrieve the value from the input and create a new li
+  //
 
-// grab the button from the DOM
-const $button = $('#add-button');
+  // grab the button from the DOM
+  const $button = $('#add-button');
 
-// add a click listener to the button
-$button.on('click', () => {
-  console.log('button got clicked');
+  // add a click listener to the button
+  $button.on('click', () => {
+    console.log('button got clicked');
 
-  // get the input field
-  const $input = $('#new-number');
+    // get the input field
+    const $input = $('#new-number');
 
-  // get the value from the input field
-  const inputContents = $input.val();
-    
-  // create a new li with the input contents
-  const newLi = $(`<li>${inputContents}</li>`);
+    // get the value from the input field
+    const inputContents = $input.val();
 
-  // add the new li to the main-list
-  $mainList.prepend(newLi);
+    // create a new li with the input contents
+    const newLi = $(`<li>${inputContents}</li>`);
 
-  // clear the input field and set focus
-  $input.val('').focus();
+    // add the new li to the main-list
+    $mainList.prepend(newLi);
+
+    // clear the input field and set focus
+    $input.val('').focus();
   // $input.focus();
+  });
+
+
 });
+
+
+
 
